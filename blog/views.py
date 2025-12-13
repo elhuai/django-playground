bf088940911aac86fe6f18fc72285670b3472774
huai <suihuai@bank.com.tw>
2025-12-13 10:35:25 +0800

    [fix] Improve error handling in article detail view and update template includes for consistency

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from blog.models import Article, Author, Tag
 
 
@@ -8,7 +8,7 @@ def article_list(request):
 
 
 def article_detail(request, article_id):
-    article = Article.objects.get(id=article_id)
+    article = get_object_or_404(Article, id=article_id)
     return render(request, "blog/article_detail.html", {"article": article})
 
 
